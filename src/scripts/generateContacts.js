a47529f3af004157063a39213571b2c1179240e7
Andriy Dzhuryn <165391426+AndriyDzhuryn@users.noreply.github.com>
2024-12-01 20:59:24 +0200

    add function generate, add one, get all, count, remove all and remove last contacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contactsList = await readContacts();
+  const newContactsList = Array(number).fill(0).map(createFakeContact);
+  const allContactsList = [...contactsList, ...newContactsList];
+  await writeContacts(allContactsList);
+};
 
 generateContacts(5);
